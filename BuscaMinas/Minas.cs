a0c2c921d3ca95bfcb4edb7703640734d278bfbc
Sebastian Germoso <sebastiangermoso01@gmail.com>
2025-11-01 08:34:22 -0400

    agrega metodo EsMina

--- BuscaMinas/Minas.cs
+++ BuscaMinas/Minas.cs
@@ -8,5 +8,22 @@ namespace BuscaMinas
 {
     internal class Minas
     {
+        public List<(int, int)> posicionMinas = new List<(int, int)>();
+
+        public void PosicionesMina(int filas, int columnas, int catidadMinas)
+        {
+            Random random = new Random();
+            while(posicionMinas.Count < catidadMinas)
+            {
+                int fila = random.Next(filas);
+                int columna = random.Next(columnas);
+                posicionMinas.Add((fila, columna));
+            }
+        }
+
+        public bool EsMina(int fila, int columna) 
+        { 
+            return posicionMinas.Contains((fila, columna));
+        }
     }
 }
