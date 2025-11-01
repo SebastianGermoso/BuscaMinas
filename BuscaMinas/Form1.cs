2e0a196c4190191144b2465a2d69cde5f313f1fb
Sebastian Germoso <sebastiangermoso01@gmail.com>
2025-11-01 08:22:17 -0400

    Agrega cuadro de juego y funcion de deteccion del click

--- BuscaMinas/Form1.cs
+++ BuscaMinas/Form1.cs
@@ -12,9 +12,62 @@ namespace BuscaMinas
 {
     public partial class Form1 : Form
     {
+        public const int fila = 10;
+        public const int columnas = 20;
+        public const int hxw = 30;
+
         public Form1()
         {
             InitializeComponent();
+            InicializarCuadro();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void InicializarCuadro()
+        {
+            for (int f = 0; f < fila; f++) 
+            { 
+                for (int c = 0; c < columnas; c++)
+                {
+                    Button cuadro = new Button();
+                    cuadro.Width = hxw;
+                    cuadro.Height = hxw;
+
+                    cuadro.Location = new Point(c * hxw, f * hxw);
+
+                    cuadro.Tag = $"{f},{c}";
+
+                    cuadro.MouseDown += new MouseEventHandler(Boton_MouseDown);
+
+                    this.Controls.Add(cuadro);
+                }
+            }
+        }
+
+        private void Boton_MouseDown(object sender, MouseEventArgs e)
+        {
+            // 1. Identificar el botón y obtener la coordenada
+            Button botonClickeado = (Button)sender;
+
+            // Obtener la cadena "f,c" del Tag y separarla
+            string[] coords = botonClickeado.Tag.ToString().Split(',');
+            int fila = int.Parse(coords[0]);
+            int columna = int.Parse(coords[1]);
+
+            // 2. Comprobar qué botón del ratón se usó
+            if (e.Button == MouseButtons.Left)
+            {
+                MessageBox.Show("izquierdo en:" + fila + ", " + columna);
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                MessageBox.Show("Derecho en:" + fila + ", " + columna);
+            }
+
         }
     }
 }
